dbc93938ef55f506eb33f98d0af43d050db14101
amarpanray <amarpanray@gmail.com>
2022-06-17 13:46:12 -0400

    Add project files.

---
+++ Models/BusinessModels/Transaction.cs
@@ -0,0 +1,14 @@
+﻿namespace BankingApp.Models.BusinessModels
+{
+    public class Transaction
+    {
+        public int TransactionID { get; set; }
+        public string? FromAccount { get; set; }
+        public string? ToAccount { get; set; } 
+        public DateTime TransactionTime { get; set; }
+
+        public double TransferAmount { get; set; }
+        public double BalanceFrom { get; set; }
+        public double BalanceTo { get; set; }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+﻿About this application 
+Uses ASP.Net CORE 6.0
+Built using Visual Studio Community 2022
+Uses Dependency Injection
+Uses Repository Pattern
