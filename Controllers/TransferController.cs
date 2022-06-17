a6cbaa95c7ee5d788df70873c52506b488266018
amarpanray <amarpanray@gmail.com>
2022-06-17 14:18:11 -0400

    Scaffolded Basic CRUD and created Database

--- Controllers/TransferController.cs
+++ Controllers/TransferController.cs
@@ -5,79 +5,84 @@ namespace BankingApp.Controllers
 {
     public class TransferController : Controller
     {
-        // GET: TransferController
-        public ActionResult Index()
+
+        public ActionResult Initiate()
         {
             return View();
         }
+        //// GET: TransferController
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
 
-        // GET: TransferController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+        //// GET: TransferController/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
 
-        // GET: TransferController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+        //// GET: TransferController/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
 
-        // POST: TransferController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //// POST: TransferController/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
 
-        // GET: TransferController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+        //// GET: TransferController/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
 
-        // POST: TransferController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //// POST: TransferController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
 
-        // GET: TransferController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+        //// GET: TransferController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
 
-        // POST: TransferController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //// POST: TransferController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
     }
 }
---
+++ Models/ViewModels/AccountViewModels.cs
@@ -0,0 +1,9 @@
+﻿namespace BankingApp.Models.ViewModels
+{
+    public class AccountViewModels
+    {
+        public int AccountID { get; set; }
+        public string? AccountName { get; set; }
+        public double Balance { get; set; }
+    }
+}
